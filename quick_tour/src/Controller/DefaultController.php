941c57986c57f15a15882217a9aa60762a21f708
Takashi Aoki <arfaben_5816@icloud.com>
2022-04-26 17:47:17 +0900

    UPDATE:同じ処理をアノテーションでの記述に変更

--- quick_tour/src/Controller/DefaultController.php
+++ quick_tour/src/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController
 {
+    /**
+     * @Route("/hello/{name}")
+     */
     public function index($name)
     {
         return new Response("Hello $name");
