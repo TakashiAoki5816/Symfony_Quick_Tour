5994d56e46f4af079ca47445ac494a85922d7057
Takashi Aoki <arfaben_5816@icloud.com>
2022-04-26 18:44:47 +0900

    UPDATE:Twigを使用したビューの表示に更新

--- quick_tour/src/Controller/DefaultController.php
+++ quick_tour/src/Controller/DefaultController.php
@@ -1,16 +1,18 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/hello/{name}")
      */
     public function index($name)
     {
-        return new Response("Hello $name");
+        return $this->render('default/index.html.twig', [
+            'name' => $name,
+        ]);
     }
 }
