333c38754d4b461d58898b7f82fca8242db1ec24
Takashi Aoki <arfaben_5816@icloud.com>
2022-04-27 16:54:34 +0900

    ADD:Loggingを記述

--- quick_tour/src/Controller/DefaultController.php
+++ quick_tour/src/Controller/DefaultController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -9,8 +10,9 @@ class DefaultController extends AbstractController
     /**
      * @Route("/hello/{name}")
      */
-    public function index($name)
+    public function index($name, LoggerInterface $logger)
     {
+        $logger->info("Saying hello to $name");
         return $this->render('default/index.html.twig', [
             'name' => $name,
         ]);
